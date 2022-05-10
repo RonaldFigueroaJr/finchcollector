0a93240ef5b0897a6200b1489cf754c1c940cc44
ronaldjr <figueroajrronald@gmail.com>
2022-05-10 08:50:43 -1000

    Added feeding lists, add function for feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,12 @@
+from argparse import _MutuallyExclusiveGroup
 from django.db import models
 from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Finch(models.Model):
@@ -7,8 +14,21 @@ class Finch(models.Model):
     family = models.CharField(max_length=100)
     description = models.TextField(max_length=600)
 
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
     def __str__(self):
-        return self.name
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
