1b770272ec59070513834702e54921f4191c9e68
ronaldjr <figueroajrronald@gmail.com>
2022-05-10 08:21:42 -1000

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def finches_index(request):
+    finches = Finch.objects.all()
+    return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+    success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['family', 'description']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
