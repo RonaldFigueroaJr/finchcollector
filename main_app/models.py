4cc729b9b680ed84001118d09eb8c29ceb3ff8a4
ronaldjr <figueroajrronald@gmail.com>
2022-05-11 09:01:57 -1000

    Added M:M Toy relationship

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
-from argparse import _MutuallyExclusiveGroup
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -8,11 +8,23 @@ MEALS = (
     ('D', 'Dinner')
 )
 
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 # Create your models here.
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     family = models.CharField(max_length=100)
     description = models.TextField(max_length=600)
+    toys = models.ManyToManyField(Toy)
 
 class Feeding(models.Model):
     date = models.DateField('feeding date')
@@ -24,11 +36,12 @@ class Feeding(models.Model):
 
     finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return f"{self.get_meal_display()} on {self.date}"
 
     class Meta:
         ordering = ['-date']
+        
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
